86a67fc8d6790c32bf9a3a2d9d35ba365363a215
DindaPasulu <dinda.prilliagrandi@student.umn.ac.id>
2021-03-07 13:52:01 +0700

    FirstCommit
    MobileApp Week 1-6

---
+++ WEEK04_33615_DINDAPRILLIAGRANDIPASULU/app/src/main/java/com/example/week04_33615_dindaprilliagrandipasulu/SecondActivity.java
@@ -0,0 +1,14 @@
+package com.example.week04_33615_dindaprilliagrandipasulu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
